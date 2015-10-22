9acc1da379010b7f007e9f470f238ca85000ea0d
Ansh <lightnarcissus@gmail.com>
2015-10-22 14:56:21 -0400

    Added interactive drawing sketch

--- localHost assets/p5JS/sketch.js
+++ localHost assets/p5JS/sketch.js
@@ -1,32 +1,142 @@
-var x = 100,
-  y = 100,
-  angle1 = 0.0,
-  segLength = 50;
 
+// All the paths
+var paths = [];
+// Are we painting?
+var painting = false;
+// How long until the next circle
+var next = 0;
+// Where are we now and where were we?
+var current;
+var previous;
 
 function setup() {
-  createCanvas(710, 400);
-  strokeWeight(20.0);
-  stroke(255, 100);
-}
+  createCanvas(displayWidth, displayHeight);
+  strokeWeight(10)
+  stroke(0);
+  current = createVector(0,0);
+  previous = createVector(0,0);
+};
 
 function draw() {
-  background(0);
+  background(200);
+  
+  // If it's time for a new point
+  if (millis() > next && painting) {
 
-  dx = mouseX - x;
-  dy = mouseY - y;
-  angle1 = atan2(dy, dx);
-  x = mouseX - (cos(angle1) * segLength);
-  y = mouseY - (sin(angle1) * segLength);
+    // Grab mouse position      
+    // current.x = mouseX;
+    // current.y = mouseY;
+    current.x=touchX;
+    current.y=touchY;
 
-  segment(x, y, angle1);
-  ellipse(x, y, 20, 20);
+    // New particle's force is based on mouse movement
+    var force = p5.Vector.sub(current, previous);
+    force.mult(0.05);
+
+    // Add new particle
+    paths[paths.length - 1].add(current, force);
+    
+    // Schedule next circle
+    next = millis() + random(100);
+
+    // Store mouse values
+    previous.x = current.x;
+    previous.y = current.y;
+  }
+
+  // Draw all paths
+  for( var i = 0; i < paths.length; i++) {
+    paths[i].update();
+    paths[i].display();
+  }
 }
 
-function segment(x, y, a) {
-  push();
-  translate(x, y);
-  rotate(a);
-  line(0, 0, segLength, 0);
-  pop();
+// Start it up
+function touchStarted()
+{
+  next = 0;
+  painting = true;
+  previous.x = touchX;
+  previous.y = touchY;
+  paths.push(new Path()); 
+}
+
+function touchEnded()
+{
+  painting=false;
+}
+// function mousePressed() {
+//   next = 0;
+//   painting = true;
+//   previous.x = mouseX;
+//   previous.y = mouseY;
+//   paths.push(new Path());
+// }
+
+// // Stop
+// function mouseReleased() {
+//   painting = false;
+// }
+
+// A Path is a list of particles
+function Path() {
+  this.particles = [];
+  this.hue = random(100);
+}
+
+Path.prototype.add = function(position, force) {
+  // Add a new particle with a position, force, and hue
+  this.particles.push(new Particle(position, force, this.hue));
+}
+
+// Display plath
+Path.prototype.update = function() {  
+  for (var i = 0; i < this.particles.length; i++) {
+    this.particles[i].update();
+  }
+}  
+
+// Display plath
+Path.prototype.display = function() {
+  
+  // Loop through backwards
+  for (var i = this.particles.length - 1; i >= 0; i--) {
+    // If we shold remove it
+    if (this.particles[i].lifespan <= 0) {
+      this.particles.splice(i, 1);
+    // Otherwise, display it
+    } else {
+      this.particles[i].display(this.particles[i+1]);
+    }
+  }
+
+}  
+
+// Particles along the path
+function Particle(position, force, hue) {
+  this.position = createVector(position.x, position.y);
+  this.velocity = createVector(force.x, force.y);
+  this.drag = 0.95;
+  this.lifespan = 255;
+}
+
+Particle.prototype.update = function() {
+  // Move it
+  this.position.add(this.velocity);
+  // Slow it down
+  this.velocity.mult(this.drag);
+  // Fade it out
+  this.lifespan--;
+}
+
+// Draw particle and connect it with a line
+// Draw a line to another
+Particle.prototype.display = function(other) {
+  stroke(0, this.lifespan);
+  fill(0, this.lifespan/2);    
+  ellipse(this.position.x,this.position.y, 8, 8);    
+  // If we need to draw a line
+  if (other) {
+    line(this.position.x, this.position.y, other.position.x, other.position.y);
+  }
 }
